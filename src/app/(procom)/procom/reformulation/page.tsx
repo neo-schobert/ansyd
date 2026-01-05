39effcf8bfa0cff6b0446719aef7e4c19bdfb378
Néo Schobert <neo.schobert@hotmail.com>
2026-01-05 16:30:11 +0100

    0.3.4

---
+++ src/app/(procom)/procom/reformulation/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <iframe
+      src="https://best-fright-078.notion.site/ebd//2aaffaf34efc80b78ac6c0a222ce197e"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+    />
+  );
+}
